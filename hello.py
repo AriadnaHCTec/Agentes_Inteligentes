e89285742a8de62f999fbeb6e26f4ac58f71e75d
AriadnaHCTec <65170456+AriadnaHCTec@users.noreply.github.com>
2021-11-29 12:43:36 -0600

    ServerIBM

--- hello.py
+++ hello.py
@@ -5,36 +5,22 @@ from TraficModel_v2 import *
 
 model = TraficModel(1, 32, 32)
 
-anterior = (0,0)
-def updatePositions(): 
-    global anterior
-    positions = []
-    positions.append(anterior)
+def updateTraffic(): 
+    states = []
     for agent in model.schedule.agents:
-        print(agent)
         if isinstance(agent,CarAgent):
-            positions.append(agent.pos)    
-            anterior = agent.pos
+            states.append(agent.pos)
     model.step()
     return positions
 
-def positionsToJSON(ps):
+def trafficToJSON(ps):
     posDICT = []
     for p in ps:
         pos = {
-            "x" : float(p[0]),
-            "z" : float(p[1]),
-            "y" : float(0.0)
+            "state" : p
         }
-        posDICT.append(pos)
-    print(json.dumps(posDICT))
+    posDICT.append(pos)    
     return json.dumps(posDICT)
-
-app = Flask(__name__, static_url_path='')
-
-# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
-# When running this app on the local machine, default the port to 8000
-port = int(os.getenv('PORT', 8000))
 
 anterior = (0,0)
 def updatePositions(): 
@@ -60,14 +46,25 @@ def positionsToJSON(ps):
     posDICT.append(pos)
     return json.dumps(posDICT)
 
+app = Flask(__name__, static_url_path='')
+
+# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
+# When running this app on the local machine, default the port to 8000
+port = int(os.getenv('PORT', 8000))
+
+
 @app.route('/')
 def root():
 	global model
 	model = TraficModel(1, 32, 32)
-    return jsonify([{"message":"Hello World from IBM Cloud!"}])
+	return jsonify([{"message":"Hello World from IBM Cloud!"}])
 
 @app.route('/updatePositions')
 def upPositions():	
+    return positionsToJSON(updatePositions())
+
+@app.route('/trafficLight')
+def upTraffic():  
     return positionsToJSON(updatePositions())
 
 if __name__ == '__main__':
